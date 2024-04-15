23eea4034cd60843f8dfccf5066318f5b9ca090a
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2024-04-15 15:29:47 -0400

    Implemented the list of transactions

--- src/app/scenes/secondPage/SecondPage.tsx
+++ src/app/scenes/secondPage/SecondPage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import {ScrollView, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import {Text} from 'react-native-paper';
 import CardControls from './CardControls';
 import CreditCardView from './CreditCardView';
 import CurrentBalance from './CurrentBalance';
 import StatementsView from './StatementsView';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Transactions from './Transactions';
 
 const SecondPage = () => {
   return (
@@ -14,6 +17,7 @@ const SecondPage = () => {
           flex: 1,
           alignItems: 'center',
           justifyContent: 'center',
+          marginBottom: 15,
         }}>
         <Text style={{fontSize: 20, fontWeight: 'bold', marginVertical: 20}}>
           Mastercard •••• 1234
@@ -22,9 +26,45 @@ const SecondPage = () => {
         <CardControls />
         <CurrentBalance />
         <StatementsView />
+        <View style={styles.recentTransactionsContainer}>
+          <Text style={styles.recentTransactionsText}>Recent Transactions</Text>
+          <TouchableOpacity
+            style={{flexDirection: 'row'}}
+            onPress={() => {
+              console.log('view clicked');
+            }}>
+            <Text style={styles.recentTransactionsSpan}>View All</Text>
+            <MaterialIcons name="chevron-right" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+        <Transactions />
       </View>
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  recentTransactionsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: 10,
+  },
+  recentTransactionsText: {
+    flex: 1,
+    fontSize: 14,
+    alignItems: 'flex-start',
+    color: '#333333',
+    fontWeight: 'bold',
+  },
+  recentTransactionsSpan: {
+    fontSize: 14,
+    alignItems: 'flex-end',
+    color: '#333333',
+    fontWeight: 'bold',
+    paddingTop: 3,
+  },
+});
 
 export default SecondPage;
---
+++ src/app/scenes/secondPage/Transactions.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {Avatar} from 'react-native-paper';
+
+import DunkinDonutsLogo from '../../../assets/images/dunkindonuts.png';
+import AmazonLogo from '../../../assets/images/amazon.png';
+import StarbucksLogo from '../../../assets/images/starbucks.png';
+import {formatCurrency} from '../../utilities/commonFunctions';
+
+const transactionList = [
+  {
+    id: 1,
+    name: 'Starbucks',
+    logo: StarbucksLogo,
+    date: new Date(),
+    owing: 5.43,
+    points: 5,
+  },
+  {
+    id: 2,
+    name: 'Amazon',
+    logo: AmazonLogo,
+    date: new Date(),
+    owing: 125.3,
+    points: 125,
+  },
+  {
+    id: 3,
+    name: 'DunkinDonuts',
+    logo: DunkinDonutsLogo,
+    date: new Date(),
+    owing: 10.84,
+    points: 10,
+  },
+];
+
+const Transactions = () => {
+  return (
+    <View>
+      {transactionList.map((item, index) => (
+        <View key={index} style={styles.container}>
+          <View style={styles.avatarContainer}>
+            <Avatar.Image source={item.logo} size={50} />
+          </View>
+          <View style={styles.infoContainer}>
+            <Text style={styles.itemName}>{item.name}</Text>
+            <Text style={styles.itemDate}>{item.date.toDateString()}</Text>
+          </View>
+          <View style={styles.owingContainer}>
+            <Text style={styles.itemOwing}>{formatCurrency(item.owing)}</Text>
+            <Text style={styles.itemPoints}>+{item.points} pts</Text>
+          </View>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    width: '100%',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  avatarContainer: {
+    marginRight: 10,
+  },
+  infoContainer: {
+    flex: 1,
+    marginRight: 10,
+  },
+  owingContainer: {
+    alignItems: 'flex-end',
+  },
+  itemName: {
+    fontSize: 16,
+    color: '#333333',
+    fontWeight: 'bold',
+  },
+  itemDate: {
+    fontSize: 14,
+    color: '#888',
+  },
+  itemOwing: {
+    fontSize: 16,
+    color: '#333333',
+    fontWeight: 'bold',
+  },
+  itemPoints: {
+    fontSize: 14,
+    color: '#618DFF',
+  },
+});
+
+export default Transactions;
